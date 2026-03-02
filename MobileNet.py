5d80b825ebba2bf514d6f580afae3dce858709c4
Ryan Adoremos <ryanadoremos@d-172-18-66-44.vpn.net.virginia.edu>
2026-03-01 19:28:37 -0500

    Complete MobileNet

--- MobileNet.py
+++ MobileNet.py
@@ -13,23 +13,45 @@ class DepthwiseSeparableConv(nn.Module):
         # stride equal to the provided stride, a number of groups equal to the provided number of input channels,
         # padding of 1, and no bias.
         # Perform ReLU6 in place.
-        raise NotImplementedError
-        
+        self.depthwise_convolution = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=in_channels,
+                kernel_size=3,
+                stride=stride,
+                padding=1,
+                groups=in_channels,
+                bias=False,
+            ),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU6(inplace=True),
+        )
+
         # TODO: Assign to an instance attribute called `pointwise_convolution` an object of type Sequential
         # constructed with 1x1 2D convolution, batch normalization, and ReLU6.
         # The convolution should have a number of input channels equal to the provided number of input channels,
         # a number of output channels equal to the provided number of output channels, stride of 1, and no bias.
         # Perform ReLU6 in place.
-        raise NotImplementedError
+        self.pointwise_convolution = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1,
+                stride=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU6(inplace=True),
+        )
 
 
     def forward(self, x):
 
         # TODO: Assign to a local variable called intermediate the output of passing the input through depthwise convolution.
-        raise NotImplementedError
+        intermediate = self.depthwise_convolution(x)
 
         # TODO: Return the output of passing intermediate through pointwise convolution.
-        raise NotImplementedError
+        return self.pointwise_convolution(intermediate)
 
 
 class InvertedResidual(nn.Module):
@@ -38,14 +60,14 @@ class InvertedResidual(nn.Module):
 
         # TODO: Assign to an instance attribute called `residual_will_be_used` an indicator
         # that stride is 1 and the number of input channels equals the number of output channels.
-        raise NotImplementedError
+        self.residual_will_be_used = (stride == 1) and (in_channels == out_channels)
 
         # TODO: Assign to a local variable called `hidden_dim`
         # the product of the provided number of input channels and the provided expansion ratio.
-        raise NotImplementedError
+        hidden_dim = int(in_channels * expand_ratio)
 
         # TODO: Create an empty list called `list_of_layers`.
-        raise NotImplementedError
+        list_of_layers = []
 
         # TODO: If the provided expansion ratio is not equal to 1,
         #     add to the list of layers 1x1 2D convolution, 2D batch normalization, and ReLU6.
@@ -53,31 +75,63 @@ class InvertedResidual(nn.Module):
         #     a number of output channels equal to the hidden dimension, and no bias.
         #     Add to the list of layers batch normalization.
         #     Perform ReLU6 in place.
-        raise NotImplementedError
+        if expand_ratio != 1:
+            list_of_layers.append(
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=hidden_dim,
+                    kernel_size=1,
+                    bias=False,
+                )
+            )
+            list_of_layers.append(nn.BatchNorm2d(hidden_dim))
+            list_of_layers.append(nn.ReLU6(inplace=True))
 
         # TODO: Add to the list of layers 3x3 2D convolution, 2D batch normalization, and ReLU6.
         # The convolution should have a number of input channels equal to the hidden dimension,
         # a number of output channels equal to the hidden dimension, stride equal to the provided stride,
         # a number of groups equal to the hidden dimension, padding of 1, and no bias.
         # Perform ReLU6 in place.
-        raise NotImplementedError
+        list_of_layers.append(
+            nn.Conv2d(
+                in_channels=hidden_dim,
+                out_channels=hidden_dim,
+                kernel_size=3,
+                stride=stride,
+                padding=1,
+                groups=hidden_dim,
+                bias=False,
+            )
+        )
+        list_of_layers.append(nn.BatchNorm2d(hidden_dim))
+        list_of_layers.append(nn.ReLU6(inplace=True))
 
         # TODO: Add to the list of layers 1x1 2D convolution and 2D batch normalization.
         # The convolution must have a number of input channels equal to the hidden dimension,
         # a number of output channels equal to the provided number of output channels, and no bias.
-        raise NotImplementedError
+        list_of_layers.append(
+            nn.Conv2d(
+                in_channels=hidden_dim,
+                out_channels=out_channels,
+                kernel_size=1,
+                bias=False,
+            )
+        )
+        list_of_layers.append(nn.BatchNorm2d(out_channels))
 
         # TODO: Add layers to an object of type Sequential.
         # Assign that object to an instance attribute called sequential.
-        raise NotImplementedError
+        self.sequential = nn.Sequential(*list_of_layers)
 
 
     def forward(self, x):
 
         # TODO: If residual will be used, return the output of adding the provided input and
         # the output of passing the provided input through sequential.
-        # Otherwise, return the output of passing the provided input through sequential.
-        raise NotImplementedError
+        # Otherwise, return the output of passing the provided input through sequentia
+        if self.residual_will_be_used:
+            return x + self.sequential(x)
+        return self.sequential(x)
 
 
 class MobileNet(nn.Module):
@@ -87,7 +141,7 @@ class MobileNet(nn.Module):
         # TODO: Define a local variable called `number_of_output_channels_in_initial_convolution`
         # equal to the product of 32 and the provided multiplier.
         # Cast the product to an integer.
-        raise NotImplementedError
+        number_of_output_channels_in_initial_convolution = int(32 * width_mult)
 
         # TODO: Assign to an instance attribute called `initial_convolution` an object of type Sequential
         # constructed with a 3x3 2D convolution, 2D batch normalization, and ReLU6.
@@ -95,10 +149,21 @@ class MobileNet(nn.Module):
         # a number of output channels equal to the defined number of output channels in the initial convolution,
         # stride of 2, padding of 1, and no bias.
         # ReLU6 should be performed in place.
-        raise NotImplementedError
+        self.initial_convolution = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=number_of_output_channels_in_initial_convolution,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(number_of_output_channels_in_initial_convolution),
+            nn.ReLU6(inplace=True),
+        )
 
         # TODO: Create an empty list of layers called `list_of_layers`.
-        raise NotImplementedError
+        list_of_layers = []
 
         configuration = [
             (32, 16, 1, 1),
@@ -131,16 +196,26 @@ class MobileNet(nn.Module):
         #     a number of input channels equal to the scaled number of input channels,
         #     a number of output channels equal to the scaled number of output channels,
         #     the appropriate stride, and the appropriate expansion ratio.
-        raise NotImplementedError
+        for number_of_input_channels, number_of_output_channels, stride, expansion_ratio in configuration:
+            scaled_number_of_input_channels = int(number_of_input_channels * width_mult)
+            scaled_number_of_output_channels = int(number_of_output_channels * width_mult)
+            list_of_layers.append(
+                InvertedResidual(
+                    in_channels=scaled_number_of_input_channels,
+                    out_channels=scaled_number_of_output_channels,
+                    stride=stride,
+                    expand_ratio=expansion_ratio,
+                )
+            )
 
         # TODO: Add layers in the list of layers to an object of type Sequential.
         # Assign that object to an instance attribute called sequential.
-        raise NotImplementedError
+        self.sequential = nn.Sequential(*list_of_layers)
 
         # TODO: Define a local variable called `number_of_output_channels_in_final_convolution`
         # equal to the product of 1280 and the provided multiplier if the multiplier is greater than 1.0 and 1280 otherwise.
         # Cast the product to an integer.
-        raise NotImplementedError
+        number_of_output_channels_in_final_convolution = int(1280 * width_mult) if width_mult > 1.0 else 1280
 
         # TODO: Assign to an instance attribute called `final_convolution` an object of type Sequential
         # constructed with a 1x1 2D convolution, 2D batch normalization, and ReLU6.
@@ -148,21 +223,30 @@ class MobileNet(nn.Module):
         # a number of output channels equal to the defined number of output channels in the final convolution, and no bias.
         # Cast the product to an integer.
         # ReLU6 should be performed in place.
-        raise NotImplementedError
+        self.final_convolution = nn.Sequential(
+            nn.Conv2d(
+                in_channels=int(320 * width_mult),
+                out_channels=number_of_output_channels_in_final_convolution,
+                kernel_size=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(number_of_output_channels_in_final_convolution),
+            nn.ReLU6(inplace=True),
+        )
 
         # TODO: Assign to an instance attribute called `average_pooling` 2D average pooling with height of 1 and width of 1.
-        raise NotImplementedError
+        self.average_pooling = nn.AdaptiveAvgPool2d((1, 1))
 
         # TODO: Assign to an instance attribute called `dropout` dropout with the provided probability.
-        raise NotImplementedError
+        self.dropout = nn.Dropout(p=dropout_prob)
 
         # TODO: Assign to an instance attribute called `linear_transformation` a linear transformation
         # with a number of input features equal to the number of output channels in the final convolution and
         # a number of output features equal to the provided number of classes.
-        raise NotImplementedError
+        self.linear_transformation = nn.Linear(number_of_output_channels_in_final_convolution, num_classes)
 
         # TODO: Call method `initialize_weights`.
-        raise NotImplementedError
+        self.initialize_weights()
 
 
     def initialize_weights(self):
@@ -178,29 +262,39 @@ class MobileNet(nn.Module):
         #     otherwise, if the module is a linear transformation,
         #         fill the module weight with values drawn from the standard normal distribution.
         #         Fill the module bias with 0.
-        raise NotImplementedError
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+            elif isinstance(module, nn.BatchNorm2d):
+                nn.init.constant_(module.weight, 1)
+                nn.init.constant_(module.bias, 0)
+            elif isinstance(module, nn.Linear):
+                nn.init.normal_(module.weight, mean=0.0, std=1.0)
+                nn.init.constant_(module.bias, 0)
 
 
     def forward(self, x):
 
         # TODO: Assign to a local variable called intermediate the output of
         # passing the provided input through the initial convolution.
-        raise NotImplementedError
+        intermediate = self.initial_convolution(x)
 
         # TODO: Assign to intermediate the output of passing intermediate through the sequential.
-        raise NotImplementedError
+        intermediate = self.sequential(intermediate)
 
         # TODO: Assign to intermediate the output of passing intermediate through the final convolution.
-        raise NotImplementedError
+        intermediate = self.final_convolution(intermediate)
 
         # TODO: Assign to intermediate the output of passing intermediate through average pooling.
-        raise NotImplementedError
+        intermediate = self.average_pooling(intermediate)
 
         # TODO: Flatten intermediate from start dimension 1 on.
-        raise NotImplementedError
+        intermediate = torch.flatten(intermediate, start_dim=1)
 
         # TODO: Assign to intermediate the output of passing intermediate through dropout.
-        raise NotImplementedError
+        intermediate = self.dropout(intermediate)
 
         # TODO: Return the output of passing intermediate through the linear transformation.
-        raise NotImplementedError
+        return self.linear_transformation(intermediate)
