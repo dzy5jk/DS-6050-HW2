c6df0fb2fa96c029e4a4478fda017c9fc0d36a7a
Ryan Adoremos <ryanadoremos@d-172-18-66-44.vpn.net.virginia.edu>
2026-03-01 19:13:38 -0500

    Complete transfer learning

--- transfer_learning.py
+++ transfer_learning.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 def get_pretrained_model(model_name='resnet18', num_classes=18, feature_extract=True):
     
     # TODO: Assign to local variable model None.
-    raise NotImplementedError
+    model = None
 
     # TODO: If model name is "resnet18",
     #    assign to model the output of function `resnet18` in module models.
@@ -18,8 +18,14 @@ def get_pretrained_model(model_name='resnet18', num_classes=18, feature_extract=
     #    Redefine the linear transformation of the model as a linear transformation with
     #    a number of input features equal to the number of input features of the existing linear transformation and
     #    a number of output features equal to the provided number of classes.
-    raise NotImplementedError
-    
+    if model_name == "resnet18":
+        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        if feature_extract:
+            for parameter in model.parameters():
+                parameter.requires_grad = False
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features, num_classes)
+
     # TODO: If model name is "vgg16",
     #    assign to model the output of function `vgg16` in module models.
     #    Function `vgg16` must receive for its parameter called weights
@@ -34,7 +40,15 @@ def get_pretrained_model(model_name='resnet18', num_classes=18, feature_extract=
     #    Redefine the layer with index 6 of the classifier of the model as a linear transformation with
     #    a number of input features equal to the number of input features of `layer_6` and
     #    a number of output features equal to the provided number of classes.
-    raise NotImplementedError
+    elif model_name == "vgg16":
+        model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+        if feature_extract:
+            for parameter in model.features.parameters():
+                parameter.requires_grad = False
+            for parameter in model.classifier.parameters():
+                parameter.requires_grad = False
+        layer_6 = cast(nn.Linear, model.classifier[6])
+        model.classifier[6] = nn.Linear(layer_6.in_features, num_classes)
 
     # TODO: If model name is "mobilenet_v2",
     #    assign to model the output of function `mobilenet_v2` in module models.
@@ -48,11 +62,18 @@ def get_pretrained_model(model_name='resnet18', num_classes=18, feature_extract=
     #    Redefine the layer with index 1 of the classifier of the model as a linear transformation with
     #    a number of input features equal to the number of input features of `layer_1` and
     #    a number of output features equal to the provided number of classes.
-    raise NotImplementedError
+    elif model_name == "mobilenet_v2":
+        model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
+        if feature_extract:
+            for parameter in model.parameters():
+                parameter.requires_grad = False
+        layer_1 = cast(nn.Linear, model.classifier[1])
+        model.classifier[1] = nn.Linear(layer_1.in_features, num_classes)
 
     # TODO: Otherwise,
     #    raise a value error with a message of "Model {model name} is unsupported.".
-    raise NotImplementedError
+    else:
+        raise ValueError(f"Model {model_name} is unsupported.")
     
     # TODO: Return the model.
-    raise NotImplementedError
+    return model
